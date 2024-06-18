668f1bdd83d0923361622f254cc034def1fa73cb
Nino-YM <119847747+Nino-YM@users.noreply.github.com>
2024-06-18 17:23:47 +0200

    réparation formulaire activité

--- src/Form/ActivitiesType.php
+++ src/Form/ActivitiesType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Activities;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Sections;
+use App\Entity\Teams;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +21,18 @@ class ActivitiesType extends AbstractType
             ->add('date', null, [
                 'widget' => 'single_text',
             ])
-        ;
+            ->add('section', EntityType::class, [
+                'class' => Sections::class,
+                'choice_label' => 'name',
+                'placeholder' => 'Choose a section',
+            ])
+            ->add('team', EntityType::class, [
+                'class' => Teams::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => false,
+                'placeholder' => 'Choose teams',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
