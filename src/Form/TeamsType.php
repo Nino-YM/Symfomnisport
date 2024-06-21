a6e4d7f941968a7cc17de64b8ac3234d01717850
Nino-YM <119847747+Nino-YM@users.noreply.github.com>
2024-06-21 12:15:31 +0200

    ajout des membres dans teams

--- src/Form/TeamsType.php
+++ src/Form/TeamsType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Teams;
+use App\Entity\Members;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,19 @@ class TeamsType extends AbstractType
     {
         $builder
             ->add('name')
-        ;
+            ->add('members', CollectionType::class, [
+                'entry_type' => EntityType::class,
+                'entry_options' => [
+                    'class' => Members::class,
+                    'choice_label' => function (Members $member) {
+                        return $member->getName() . ' ' . $member->getLastName();
+                    },
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'label' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
